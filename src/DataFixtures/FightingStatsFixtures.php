ba02525a37408a4b0b9e03f6363c8993d2736000
Mehdi Saber <m1d3s@protonmail.com>
2022-01-14 15:39:28 +0100

    Bet, FightingStats & Invoice Fixtures

---
+++ src/DataFixtures/FightingStatsFixtures.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\FightingStats;
+use App\Entity\User;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
+use Doctrine\Persistence\ObjectManager;
+use Faker\Factory;
+
+class FightingStatsFixtures extends Fixture implements DependentFixtureInterface
+{
+    public function load(ObjectManager $manager): void
+    {
+        $faker = Factory::create();
+        $fighters = $manager->getRepository(User::class)->findByRoleNew("ROLE_FIGHTER");
+        foreach ($fighters as $fighter) {
+            $object = (new FightingStats())
+                ->setVictories($faker->numberBetween(0, 100))
+                ->setDefeats($faker->numberBetween(0, 100))
+                ->setRankingPoints($faker->randomDigit())
+                ->setTarget($fighter);
+            $fighter->setFightingStats($object);
+            $manager->persist($object);
+        }
+        $manager->flush();
+    }
+
+    public function getDependencies()
+    {
+        return [
+            UserFixtures::class
+        ];
+    }
+}
---
+++ src/DataFixtures/InvoiceFixtures.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Bet;
+use App\Entity\Invoice;
+use App\Entity\User;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
+use Doctrine\Persistence\ObjectManager;
+use Faker\Factory;
+
+class InvoiceFixtures extends Fixture implements DependentFixtureInterface
+{
+    public function load(ObjectManager $manager): void
+    {
+        // Pour le moment, on va dire: 1$ = 100 cents = 100 crédits.
+        // TODO: se mettre d'accord sur le prix final des crédits avec tout le monde.
+        // TODO: et changer le type de price en float : https://developer.paypal.com/api/payments/v2/#definition-money
+
+        $faker = Factory::create();
+        $users = $manager->getRepository(User::class)->findAll();
+
+        foreach ($users as $user) {
+            $priceInDollars = $faker->numberBetween(1, 1000);
+
+
+            $object = (new Invoice())
+                ->setDate($faker->dateTimeBetween('-1 week', '+1 week'))
+                ->setCreditAmount($priceInDollars * 100)
+                ->setPrice($priceInDollars * 100)
+                ->setIdPaypal($faker->uuid());
+
+            $user->addInvoice($object);
+            $user->setCredits($user->getCredits() + $priceInDollars * 100);
+
+            $manager->persist($object);
+        }
+
+
+        $manager->flush();
+    }
+
+    public function getDependencies()
+    {
+        return [
+            UserFixtures::class
+        ];
+    }
+}
