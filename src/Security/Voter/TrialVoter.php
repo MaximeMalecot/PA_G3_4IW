1c7a14040d13b2a3e5b43bc4ade4276774d04296
MaximeMalecot <92maximemalecot@gmail.com>
2022-02-07 14:14:34 +0100

    Voter created for trial, need to think about it and create for other entities

---
+++ src/Security/Voter/TrialVoter.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Security\Voter;
+
+use App\Entity\Trial;
+use App\Entity\User;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Core\Authorization\Voter\Voter;
+use Symfony\Component\Security\Core\User\UserInterface;
+
+class TrialVoter extends Voter
+{
+    /*
+        TO IMPLEMENT THE VOTER IN A CONTROLLER JUST DO :
+        #[IsGranted(TrialVoter::EDIT, 'trial')]
+    */
+    const EDIT = 'edit';
+    const DELETE = 'delete';
+
+    protected function supports(string $attribute, $subject): bool
+    {
+        return in_array($attribute, [self::EDIT, self::DELETE])
+            && $subject instanceof Trial;
+    }
+
+    protected function voteOnAttribute(string $attribute, $subject, TokenInterface $token): bool
+    {
+        /** @var User $user */
+        $user = $token->getUser();
+        if (!$user instanceof UserInterface) {
+            return false;
+        }
+
+        switch ($attribute) {
+            case self::EDIT:
+                return $this->canEdit($subject, $user);
+                break;
+            case self::DELETE:
+                return in_array('ROLE_ADMIN', $user->getRoles())    || $this->canEdit($subject, $user);
+                break;
+        }
+
+        return false;
+    }
+
+    /**
+     * @param Trial $trial
+     * @param User $user
+     * @return bool
+     */
+    protected function canEdit(Trial $trial, User $user): bool
+    {
+        if( $trial->getFighters()->contains($user) || $user == $trial->getAdjudicate())
+        {
+            return true;
+        }
+        return false;
+    }
+}
