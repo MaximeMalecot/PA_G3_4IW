f6071bbbd024f2179e17b41531cec14906307c79
Mehdi Saber <m1d3s@protonmail.com>
2022-04-04 13:29:57 +0200

    fix: redirect registered user to login page instead of profiler

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -48,7 +48,7 @@ class RegistrationController extends AbstractController
             $entityManager->flush();
             $ues->sendEmailConfirmation($user);
             
-            return $this->redirectToRoute('_profiler_home');
+            return $this->redirectToRoute('app_login');
         }
 
         return $this->render('registration/register.html.twig', [
