3218ddb5e7b11b09e4af97738a074b07ecca0373
jehanebnj <jehanebnj@gmail.com>
2022-07-05 00:14:21 +0200

    verification payment

--- src/Controller/Front/PaymentController.php
+++ src/Controller/Front/PaymentController.php
@@ -24,12 +24,12 @@ class PaymentController extends AbstractController
     #[Route('/payment/{credit}', name: 'payment')]
     public function payment(int $credit): Response
     {
-        $securityContext = $this->container->get('security.authorization_checker');
+        // $securityContext = $this->container->get('security.authorization_checker');
 
-        if ($securityContext->isGranted('IS_AUTHENTICATED_REMEMBERED') === false) {
+        // if ($securityContext->isGranted('IS_AUTHENTICATED_REMEMBERED') === false) {
 
-            return $this->redirect("/login");
-        }
+        //     return $this->redirect("/login");
+        // }
 
         $credits = [ 100 => "10", 250 => "25", 500 => "50", 750 => "65"];
 
@@ -40,8 +40,7 @@ class PaymentController extends AbstractController
         $session = new Session();
     
         $price = $credits[$credit];
-        $session->set('credit', $credit);
-
+       
         \Stripe\Stripe::setApiKey('sk_test_51LGVfsBYnbPwVzITdZ1beyU8wGKOFIZDYQNHbysLI7wof5e2n3SPGhdkPVsOvkzsfFWnb8btlVhoCuG5X3Kk1OqA004NNlVIXq');
         $YOUR_DOMAIN = 'http://localhost:81/';
         $checkout_session = \Stripe\Checkout\Session::create([
@@ -61,24 +60,50 @@ class PaymentController extends AbstractController
             'success_url' => $YOUR_DOMAIN . 'success',
             'cancel_url' => $YOUR_DOMAIN . 'cancel',
         ]);
+        
+        $session->set('credit', $credit);
+        $session->set('payment_intent', $checkout_session["payment_intent"]);
+
         return $this->redirect($checkout_session->url);
     }
 
     #[Route('/success', name: 'success')]
     public function success(Request $request, UserRepository $userRepository): Response
     {
-        $entityManager = $this->getDoctrine()->getManager();
-        $user = $entityManager->getRepository(User::class)->find(129);
-        //$userConnected = $this->get('security.token_storage')->getToken()->getUser();
-        $session = new Session();
-        $credits = $user->getCredits();
-        $credits += $session->get('credit');
 
-        $user->setCredits($credits);
-        $entityManager->flush();
-        $session->remove('credit');
-      
-        return $this->render('front/payment/success.html.twig');
+        $session = new Session();
+
+        if ( $session->get('payment_intent') == null ){
+            return $this->redirect("/login");
+        }
+
+        $stripe = new \Stripe\StripeClient(
+            'sk_test_51LGVfsBYnbPwVzITdZ1beyU8wGKOFIZDYQNHbysLI7wof5e2n3SPGhdkPVsOvkzsfFWnb8btlVhoCuG5X3Kk1OqA004NNlVIXq'
+        );
+        
+        $payment_intent = $stripe->paymentIntents->retrieve(
+            $session->get('payment_intent'),
+            []
+        );
+
+        //$userConnected = $this->get('security.token_storage')->getToken()->getUser();
+       if ($payment_intent->status == "succeeded"){
+
+            $entityManager = $this->getDoctrine()->getManager();
+            $user = $entityManager->getRepository(User::class)->find(129);
+            $credits = $user->getCredits();
+            $credits += $session->get('credit');
+            $user->setCredits($credits);
+
+            $entityManager->flush();
+            $session->remove('credit');
+            $session->remove('payment_intent');
+          
+            return $this->render('front/payment/success.html.twig');
+       }else{
+        return $this->render('front/payment/cancel.html.twig');
+       }
+
     }
 
     #[Route('/cancel', name: 'cancel')]
