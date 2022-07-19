be1bde0fb68c7c5b9a576336be0a63690ad71301
Mehdi Saber <m1d3s@protonmail.com>
2022-07-19 18:23:26 +0200

    Peuplement de la page par défaut avec des statistiques

--- src/Controller/Back/DefaultController.php
+++ src/Controller/Back/DefaultController.php
@@ -6,6 +6,7 @@ use App\Entity\Tournament;
 use App\Repository\TicketRepository;
 use App\Repository\TournamentRepository;
 use App\Repository\TrialRepository;
+use App\Repository\UserRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -13,17 +14,21 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 class DefaultController extends AbstractController
 {
     #[Route('/', name: 'default')]
-    public function index(TrialRepository $trialRepository, TournamentRepository $tournamentRepository, TicketRepository $ticketRepository): Response
+    public function index(TrialRepository $trialRepository, TournamentRepository $tournamentRepository, TicketRepository $ticketRepository, UserRepository $userRepository): Response
     {
         $user = $this->getUser();
         if(in_array('ROLE_ADJUDICATE', $user->getRoles())){
             return $this->render('back/default/index.html.twig', [
                 'trials' => $trialRepository->findBy(['adjudicate' => $user, 'status' => 'AWAITING'], ['dateStart' => 'ASC'], 5, 0),
-                'tournaments' => $tournamentRepository->findAjudicatedTournaments($user)
+                'tournaments' => $tournamentRepository->findAjudicatedTournaments($user),
+                'userCount' => count($userRepository->findAll())
             ]);
         }else{
             return $this->render('back/default/index.html.twig', [
                 'tickets' => 'BACK',
+                'userCount' => count($userRepository->findAll()),
+                'awaitingTrialCount' => count($trialRepository->findBy(['status' => 'AWAITING'])),
+                'awaitingTournamentCount' => count($tournamentRepository->findBy(['status' => 'AWAITING'])),
             ]);
         }
     }
